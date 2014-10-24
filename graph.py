54b8104bf21a89448bbc661c9ee4dfd825169674
Larion Garaczi <l4rion@gmail.com>
2014-10-25 01:08:49 +0200

    Added strongly connected components function to the graph algorithms + some minor refactorization in graph.py

--- graph.py
+++ graph.py
@@ -5,6 +5,8 @@
 The algorithms that are implemented in this module are:
     c_com(graph) - find the connected components in an undirected graph
 
+    scc(graph) - find all strongly connected components in an directed graph
+
     cycles(graph) - find all cycles in an undirected graph
 
     rand_graph(n, p) - return a random undirected graph and 
@@ -16,75 +18,31 @@ The algorithms that are implemented in this module are:
 Author: Larion Garaczi
 Date: 2014
 """
-    # TODO
-    # scc(graph) - find the strongly connected components of a directed graph
-    #
-    # kruskal(graph) - find the minimum spanning tree (for sparse graphs)
-    #
-    # prim(graph) - find the minimum spanning tree (for dense graphs)
-    #
-    # largest_perm(graph) - find the maximum permutation in a bipartite graph
-    #
-    # color(graph, n) - find an n-coloring for the graph
-    #
-    # shortest_path(graph, x, y) - find the shortest path between the nodes
-    # x and y in an unweighted graph
-    #
-    # dijkstra(graph, x, y) - find the shortest path between two nodes in a
-    # weighted graph
-    #
-    # bellman_ford(graph, x, y) - find the shortest path between two nodes in a
-    # weighted graph
-    #
-    # articulaton_vertex(graph) - find the articulation vertices in a graph
-    #
-
 import random
 import graphsearch
 
 from algoyoga_test import BaseTest
 
-def cycles(graph):
-    """ Return the cycles in an undirected graph. """
-    cycles = []
-    def process_edge(s_state, x, y):
-        cycle = []
-        if y in s_state.discovered and y not in s_state.processed and s_state.parents[x] != y:
-            # We found a back edge; now we just need to back up from y until 
-            # we find x to get the cycle
-            # print x, y
-            z = x
-            while z!=y:
-                cycle.append(z)
-                z=s_state.parents[z]
-            cycle.append(y)
-            cycles.append(list(reversed(cycle)))
-    graphsearch.search(graph, search_type="dfs", process_edge = process_edge)
-    return cycles
+### interface ###
 
-def c_com(graph):
-    """ Take an undirected graph represented as an adjacency list (a dict)
-    and return its connected components (as a list of sets).
-    """
-    comp_members = set() # current component members
-    comps = [] # list containing all the components
+def cycles(input_graph):
+    """ Wrapper around Graph.cycles """
+    gr = Graph(input_graph)
+    return gr.cycles()
 
-    def push_stack(s_state, node):
-        comp_members.add(node)
+def c_com(input_graph):
+    """ Wrapper around Graph.c_com """
+    gr = Graph(input_graph)
+    return gr.c_com()
 
-    def pop_stack(s_state, node):
-        component = comp_members.copy()
-        comp_members.clear()
-        if component: # a component can't be empty
-            comps.append(component) # add component to the component list
-
-    graphsearch.search(graph, search_type="bfs", process_vertex_early = push_stack, new_component = pop_stack)
-    pop_stack(None, None) # add the last component
-    return comps
+def scc(input_graph):
+    """ Wrapper around Graph.scc """
+    gr = Graph(input_graph)
+    return gr.scc()
 
 def rand_graph(n, p):
-    """ generate a random undirected graph with n nodes and an
-    edge probability of p
+    """ Generate a random undirected graph with n nodes and an
+    edge probability of p.
     """
     graph = defaultdict(list)
     for i in range(n):
@@ -93,12 +51,11 @@ def rand_graph(n, p):
             if random.random()<=p:
                 graph[i].append(i2)
                 graph[i2].append(i)
-
     return graph
 
 def rand_dgraph(n, p):
-    """ generate a random directed graph with n nodes and an
-    edge probability of p 
+    """ Generate a random directed graph with n nodes and an
+    edge probability of p.
     """
     graph = dict()
     for i in range(n):
@@ -108,16 +65,39 @@ def rand_dgraph(n, p):
                 graph[i].append(i2)
     return graph
 
+#################
+
+# TODO
+# kruskal(graph) - find the minimum spanning tree (for sparse graphs)
+#
+# prim(graph) - find the minimum spanning tree (for dense graphs)
+#
+# largest_perm(graph) - find the maximum permutation in a bipartite graph
+#
+# color(graph, n) - find an n-coloring for the graph
+#
+# shortest_path(graph, x, y) - find the shortest path between the nodes
+# x and y in an unweighted graph
+#
+# dijkstra(graph, x, y) - find the shortest path between two nodes in a
+# weighted graph
+#
+# bellman_ford(graph, x, y) - find the shortest path between two nodes in a
+# weighted graph
+#
+# articulaton_vertex(graph) - find the articulation vertices in a graph
+#
+
 # TODO
 def largest_perm(graph): #TODO: update docstring -> relation between natural numbers
     """ Takes an arbitrary relation R in {1, 2, ..., n}^2. The relation is represented as a
     tuple (a_1, a_2, a_3) where a_n is the list of numbers m for which Rnm. Return the
     largest subrelation that represents a permutation of some subset L of {1, 2, ..., n}.
-    Or equivalently return the largest bijection, which is a subrelation of R.
+    (Or - equivalently - return the largest bijection which is also a subrelation of R.)
     """ 
     perm = graph[:]
     counts = [len([n_list for n_list in graph if n in n_list]) for n in range(len(graph))]
-    print counts
+    print(counts)
     try:
         next_ind = counts.index(0)
     except ValueError:
@@ -132,10 +112,142 @@ def largest_perm(graph): #TODO: update docstring -> relation between natural num
         except ValueError:
             return perm
 
+class Graph(object):
+    """ Class for the graph algorithms. """
+    def __init__(self, graph):
+        """ Initialize the Graph object. This method just stores the graph to be 
+        processed. 
+        """
+        self.graph = graph
+
+    def scc(self):
+        """ Take a directed graph represented as an adjacency list (a dict
+        mapping vertices to their neighbours) and return its strongly
+        connected components.
+        """
+        class searchglobals:
+            clock = 0 # initialize clock
+            dt = dict() # map nodes to their discovery time
+            # map a node X to the lowest dt[Y] s. t. Y is reachable from X
+            reachable_dt = dict() 
+            stack = []
+            components = list() 
+
+        def proc_vertex_early(s_state, node):
+            # initialize dt and reachable_dt (every node is trivially reachable from itself)
+            searchglobals.reachable_dt[node] = searchglobals.dt[node] = searchglobals.clock
+            searchglobals.clock+=1 # tick
+            searchglobals.stack.append(node)
+
+        def proc_edge(s_state, x, y): 
+            if y in s_state.discovered and y not in s_state.processed:
+                # found back-edge, update reachable_dt accordingly
+                if searchglobals.reachable_dt[x] > searchglobals.dt[y]:
+                    searchglobals.reachable_dt[x] = searchglobals.dt[y]
+
+        def proc_vertex_late(s_state, node): 
+            parent = s_state.parents[node]
+            reachable = searchglobals.reachable_dt[node] 
+            dt = searchglobals.dt[node]
+            # propagate reachable_dt upwards
+            if parent is not None and reachable < searchglobals.reachable_dt[parent]:
+                searchglobals.reachable_dt[parent] = reachable
+            if reachable == dt:
+                # found a component, pop it out from the stack
+                comp = set()
+                comp_node = searchglobals.stack.pop()
+                comp.add(comp_node)
+                while comp_node != node:
+                    comp_node = searchglobals.stack.pop()
+                    comp.add(comp_node)
+                searchglobals.components.append(comp)
+
+        graphsearch.search(self.graph, search_type="dfs", process_vertex_early = proc_vertex_early,
+                process_edge = proc_edge, process_vertex_late = proc_vertex_late)
+        return searchglobals.components
+
+    def cycles(self):
+        """ Return the cycles in an undirected graph. """
+        graph = self.graph
+        cycles = []
+        def process_edge(s_state, x, y):
+            cycle = []
+            if y in s_state.discovered and y not in s_state.processed and s_state.parents[x] != y:
+                # We found a back edge; now we just need to back up from y until 
+                # we find x to get the cycle
+                # print x, y
+                z = x
+                while z!=y:
+                    cycle.append(z)
+                    z=s_state.parents[z]
+                cycle.append(y)
+                cycles.append(list(reversed(cycle)))
+        graphsearch.search(graph, search_type="dfs", process_edge = process_edge)
+        return cycles
+
+    def c_com(self):
+        """ Take an undirected graph represented as an adjacency list (a dict)
+        and return its connected components (as a list of sets).
+        """
+        graph = self.graph
+        comp_members = set() # current component members
+        comps = [] # list containing all the components
+        def push_stack(s_state, node):
+            comp_members.add(node)
+        def pop_stack(s_state, node):
+            component = comp_members.copy()
+            comp_members.clear()
+            if component: # a component can't be empty
+                comps.append(component) # add component to the component list
+        graphsearch.search(graph, search_type="bfs", process_vertex_early = push_stack, new_component = pop_stack)
+        pop_stack(None, None) # add the last component
+        return comps
+
 class GraphTest(BaseTest):
     def __init__(self):
-        testlist = [self.test_ccom, self.test_cycles]
+        testlist = [self.test_ccom, self.test_cycles, self.test_scc]
         super(GraphTest,self).__init__("miscellaneous graph algorithms", testlist)
+
+    def test_scc(self):
+        """ Test the scc function (strongly connected components). """
+        complex_graph = {
+                1: [2], # first scc
+                2: [3, 5],
+                3: [4],
+                4: [1],
+                5: [6, 7], # second scc
+                6: [5, 7],
+                7: [8], # third scc
+                8: [9], # fourth scc
+                9: [10],
+                10: [8],
+                11: [12], # fifth scc
+                12: [], # sixth scc
+                13: [14], # seventh scc
+                14: [13],
+                }
+        complex_sccs = frozenset([
+                frozenset([1,2,3,4]),
+                frozenset([5,6]),
+                frozenset([7]),
+                frozenset([8,9,10]),
+                frozenset([11]),
+                frozenset([12]),
+                frozenset([13,14]),
+                ])
+        triv = dict()
+        triv_sccs = frozenset()
+        large_cycle = {n:[(n+1)%100] for n in range(100)}
+        large_cycle_sccs = frozenset([frozenset(range(100))])
+        testcases = [ 
+                (complex_graph, complex_sccs),
+                (triv, triv_sccs),
+                (large_cycle, large_cycle_sccs),
+                ]
+        for (input_graph, expected) in testcases:
+            result = scc(input_graph)
+            assert frozenset([frozenset(component) for component in result]) == expected
+        return "test pass"
 
     def test_ccom(self):
         """ test c_com (connected components in an undirected graph) function """
